df492284cf89b7613def15cf5b64b29ae99bb999
Akira Kawai <kawaiakira51@gmail.com>
2022-09-16 16:10:57 +0900

    notion top page id changes

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './.lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cc7726dd617345d1813bda034d4da4d8',
+  rootNotionPageId: 'd2d1d1def5204de7b0a875d0867f8c6d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
